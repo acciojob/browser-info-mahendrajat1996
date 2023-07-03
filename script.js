1c765dbbbd7f3d6064010f1a63206e18c6d60341
mahendrajat1996 <130475100+mahendrajat1996@users.noreply.github.com>
2023-07-03 22:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function getBrowserInfo() {
+        const browserInfoElement = document.getElementById("browser-info");
+        const browserName = navigator.appName;
+        const version = navigator.appVersion;
+        const message = "You are using " + browserName + " version " + version;
+        browserInfoElement.textContent = message;
+      }
+
+      // Call the function to get and display the browser information
+      getBrowserInfo();
